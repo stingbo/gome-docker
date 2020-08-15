5edc4643d9336ad41ba234119834be32c10db5f7
wanlianbo <lianbo.wan@gmail.com>
2020-08-15 21:15:16 +0800

    Golang+gRPC+ProtoBuf+RabbitMQ+Redis撮合引擎 init

---
+++ README.md
@@ -0,0 +1,5 @@
+##### Golang+gRPC+ProtoBuf+RabbitMQ+Redis撮合引擎
+
+1. `copy docker-compose.yml.example docker-compose.yml`
+
+2. `docker-compose up -d`
---
+++ golang/start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#coding=utf-8
+#作用: 本脚本旨在在容器启动后把配置文件和需要启动程序进行配置和创建
+
+#export GO111MODULE="on"
+#export GOPROXY="https://goproxy.cn"
+
+#run config
+#cp ./supervisord.conf /etc/supervisor/
+
+#create path for log
+#mkdir -p /var/log/your_need_dic
+#chmod -R 777 /var/log/your_need_dic
+
+#start supervisord
+#/usr/bin/supervisord
+
+#tail -f /dev/null
